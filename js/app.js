7797487c239bfe6c6f89de3932d5339a059ec0db
AlaaElsayed14 <alaaelsayed1433@gmail.com>
2022-06-14 17:17:03 +0200

    bootstrap templete

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// const closeIcon = document.querySelector(".icon-close");
+// const closeButton = document.querySelector("#closeBtn");
+
+// const loginBtn = document.querySelector(".loginBtn");
+// const loginForm = document.querySelector(".login");
+
+// loginBtn.addEventListener("click" , function(e){e.preventDefault();
+//   show();
+//  } );
+
+// function show(){
+//   loginForm.classList.toggle("loginShow");
+// }
+
+// $(document).ready(function(){ 
+//     $("#loginBtn").click(function() {
+//     $(".login").addClass("loginShow")
+   
+//     })
+// })
